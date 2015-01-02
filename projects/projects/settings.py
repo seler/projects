f0c97778f76bfbf7cc066c143e47d83a2897e086
Rafał Selewońko <rafal@selewonko.com>
2015-01-02 07:36:07 +0100

    Django registration forms, navbar with login info, login and logout features

--- projects/projects/settings.py
+++ projects/projects/settings.py
@@ -91,3 +91,14 @@ STATICFILES_DIRS = [
 # set this up in prod environment!
 # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.contrib.auth.context_processors.auth',
+    'django.core.context_processors.request',
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.core.context_processors.tz',
+    'django.contrib.messages.context_processors.messages'
+)
